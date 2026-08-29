f705977bb9027a9ae521bfb3b76657411f131b8c
Mad112233m-cyber <Mad112233m@hotmail.com>
2026-08-29 14:18:58 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-// 1) روحي لـ https://console.firebase.google.com
-// 2) أنشئي مشروع جديد (مجاني)
-// 3) من إعدادات المشروع > عام > "أضف تطبيق ويب"
-// 4) انسخي القيم اللي تطلع لك وحطيها هنا مكان كل PASTE_...
 const firebaseConfig = {
   apiKey: "AIzaSyD6kgiZiPt6As1YzsL5GXIm7fFsoMKEQhM",
   authDomain: "mstafed-2db89.firebaseapp.com",
@@ -16,3 +13,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
